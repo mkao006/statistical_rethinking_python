f08abe0b73bb41b5133ec75e969ff06ede45b750
Michael C. J. Kao <mkao006@gmail.com>
2021-01-25 21:36:58 +1100

    added quadratic approximation using AutoLaplaceApproximation guide

--- src/base.py
+++ src/base.py
@@ -2,6 +2,14 @@ import numpy as np
 from scipy.stats import binom
 import matplotlib.pyplot as plt
 
+import torch
+from torch.distributions import constraints
+import pyro
+import pyro.distributions as dist
+from pyro.infer.autoguide import AutoLaplaceApproximation
+from pyro.infer import SVI
+from pyro.infer import Trace_ELBO
+
 class BinomialGridApproximator():
     def __init__(self, X, N, sample_size, prior_type):
         self.X = X
@@ -20,8 +28,8 @@ class BinomialGridApproximator():
             return np.repeat(1, self.sample_size)
         elif self.prior_type == 'positive':
             return (self.param_grid > 0.5).astype(int)
-        elif self.prior_type == 'exponential':
-            return np.exp(-5 * abs(self.param_grid - 0.5))
+        elif self.prior_type == 'laplace':
+            return np.exp(-0.5 * abs(self.param_grid - 0.5))
         else:
             raise NotImplementedError
 
@@ -46,18 +54,50 @@ class BinomialGridApproximator():
         posterior = self.prior * self.likelihood
         return posterior/posterior.sum()
 
+
 class BinomialQuadraticApproximator():
-    def __init__(self, X, N):
+    def __init__(self, X, N, n_steps, learning_rate, prior_type):
         self.X = X
         self.N = N
+        self.n_steps = n_steps
+        self.prior_type = prior_type
 
-        self.prior = self._generate_prior()
-        self.likelihood = binom.pmf(X, N, self.param_grid)
-        self.posterior = self._compute_posterior()
+        self.optimiser = pyro.optim.Adam({'lr': learning_rate})
+        self.map_guide = AutoLaplaceApproximation(self.model)
 
     def plot(self):
-        pass
+        plt.subplot(3, 1, 1)
+        plt.plot(self.losses)
+        plt.title('losses')
 
-    def _compute_posterior(self):
-        # estimate the mode using MAP
-        pass
+        plt.subplot(3, 1, 2)
+        plt.plot(self._posterior_approximate_mean)
+        plt.title('Posterior Mean')
+
+        plt.subplot(3, 1, 3)
+        plt.plot(self._posterior_approximate_scale)
+        plt.title('Posterior Scale (Variance)')
+
+    def train(self):
+        pyro.clear_param_store()
+        svi = SVI(self.model, self.map_guide, self.optimiser, Trace_ELBO())
+
+        self.losses = []
+        self._posterior_approximate_mean = []
+        self._posterior_approximate_scale = []
+        for step in range(self.n_steps):
+            loss = svi.step(self.X)
+            self.losses.append(loss)
+            quadratic_approximation = self.map_guide.laplace_approximation(self.X).get_posterior()
+            self._posterior_approximate_mean.append(quadratic_approximation.loc.item())
+            self._posterior_approximate_scale.append(quadratic_approximation.scale_tril.item())
+
+            if step % 50 == 0:
+                print('[iter {}]  loss: {:.4f}'.format(step, loss))
+
+    def model(self, data):
+        if self.prior_type == 'uniform':
+            p = pyro.sample('p', dist.Uniform(0, 1))
+        elif self.prior_type == 'beta':
+            p = pyro.sample('p', dist.Beta(10.0, 10.0))
+        return pyro.sample('obs', dist.Binomial(total_count=self.N, probs=p), obs=self.X)
